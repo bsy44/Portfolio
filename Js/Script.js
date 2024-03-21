50d43a73fd2d32abd01d55273d706a0cffa206ea
bsy44 <syboubacar444@gmail.com>
2024-03-22 00:51:39 +0100

    commit modal v2

--- Js/Script.js
+++ Js/Script.js
@@ -48,3 +48,32 @@ function animationPage(){
 }
 
 animationPage();
+
+function showProjectDetails() {
+    const links = document.querySelectorAll('.card__link');
+    const modals = document.querySelectorAll('.modal');
+    const btns = document.querySelectorAll('.modal__close');
+  
+    const hideModals = () => {
+      modals.forEach(modal => {
+        modal.classList.remove('show');
+      });
+    }
+  
+    links.forEach(elem => {
+      elem.addEventListener('click', (event) => {
+        event.preventDefault();
+  
+        document.querySelector(`[id=${elem.dataset.id}]`).classList.add('show');
+      });
+    });
+  
+    btns.forEach(btn => {
+      btn.addEventListener('click', (event) => {
+        hideModals();
+      });
+    });
+  
+  }
+  
+  showProjectDetails();
