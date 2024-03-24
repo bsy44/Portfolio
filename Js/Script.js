c9d62b87b92941f9c893834ac8eb6101881b59d6
bsy44 <syboubacar444@gmail.com>
2024-03-24 04:30:42 +0100

    ajout class active

--- Js/Script.js
+++ Js/Script.js
@@ -76,3 +76,26 @@ function showProjectDetails() {
 }
   
 showProjectDetails();
+
+function active(){
+  const sections = document.querySelectorAll('section');
+  const links = document.querySelectorAll('.navbar a');
+
+  window.onscroll = () => {
+    sections.forEach(section => {
+      const top = window.scrollY;
+      const offset = section.offsetTop;
+      const height = section.offsetHeight;
+      const id = section.getAttribute('id');
+      
+      if(top >= offset && top < (offset + height)){
+        links.forEach(lnk => {
+          lnk.classList.remove('active');
+          document.querySelector('header nav a[href*=' + id + ']').classList.add('active');        
+        });
+      }
+    });
+  }
+}
+
+active();
